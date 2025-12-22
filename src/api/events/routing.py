5d07402ec7219d82c8007ff0d5689b0cb3b37fa4
Rimjhim Jha <rimmijha2608@gmail.com>
2025-12-22 14:04:30 +0530

    Add notebook for testing analytics API

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
+from .schema import EventSchema,EventListSchema
 
 router  = APIRouter()
 
 @router.get("/")
-def read_events():
-    return {1,2,3}
+def read_events() -> EventListSchema:
+    return {"results": [{"id": 1}, {"id": 2}]}
 
 
 @router.get("/{event_id}")
-def get_events(event_id: int):
+def get_events(event_id: int) ->EventSchema:
     return {"id": event_id}
---
+++ src/api/events/schema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import List
+
+class EventSchema(BaseModel):
+    id: int
+
+
+#{"id" : 12}
+
+class EventListSchema(BaseModel):
+    results: List[EventSchema]
